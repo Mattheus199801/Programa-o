9608a65507063975dc7cfb01b55b3e8d92f42fc4
Matheus Nunes <matheus.brisanet@gmail.com>
2026-07-29 18:02:32 -0300

    Foi criado 3 pasta (html/css/js) para controle de teste

---
+++ git/3-Etiquetagem.js
@@ -0,0 +1,8 @@
+
+function myF(){
+    let x = 'o Git ';
+    let y = 'teste vai começa';
+    const z = x + y;
+    document.getElementById('demo0').innerText = 'A brincadeira vai começar com ' + z;
+}
+myF()
