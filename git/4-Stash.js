0d785d7d0275be5d6e35509c6c74beaccf5eb2af
Matheus Nunes <matheus.brisanet@gmail.com>
2026-07-29 18:30:59 -0300

    Foram criado 3 pasta para aprimora o trabalho (html/css/js) 4-Stash...

---
+++ git/4-Stash.js
@@ -0,0 +1,8 @@
+
+function myF(){
+    let x = 'o teste';
+    let y = ' Vai começar';
+    const z = x + y;
+    document.getElementById('demo0').innerText = 'O verdaeiro teste vai começar ' + z;
+}
+myF()
