422c0c57c6169035dc984a8a2ec81fdb22fa2cf5
Matheus Nunes <matheus.brisanet@gmail.com>
2026-08-16 10:55:43 -0300

    Na pasta W3Schools foi criado três arquivos 10-Operadores.html/css/js

---
+++ w3schools/javascript/10-Operadores.js
@@ -0,0 +1,134 @@
+function myF(){
+    let a = 10;
+    document.getElementById('sombra0').innerHTML = "O operador = é " + a;
+}
+myF();
+
+function myFS(){
+    let b = 5;
+    let c = 6;
+    let d = b + c;
+    document.getElementById('sombra1'). innerHTML = "O operador + é " + d;
+}
+myFS();
+
+function myFSf(){
+    let xa = 5;
+    let xb = 6;
+    let xc = xa * xb;
+    document.getElementById('sombra2').innerHTML = "O Operador * é " + xc;
+}
+myFSf();
+
+function muP(){
+    let gh = 5;
+    document.getElementById('sombra3').innerHTML = " O operador > (maior que) é " + (gh > 8);
+}
+muP();
+
+function myA(){
+    let xx = 3;
+    let xy = (100 + 50) * 3;
+    document.getElementById('sombra4').innerHTML = " Operações Aritméticas é " + xy;
+}
+myA();
+
+function adicao(){
+    let tex1 = "Matheus ";
+    let tex2 = " Nunez ";
+    let tex3 = tex1 + " " + tex2;
+    document.getElementById('sombra5').innerHTML = tex3;
+}
+adicao();
+
+function atribuicao(){
+    let xd = 10;
+    xd += 5;
+    document.getElementById('sombra6').innerHTML = "O operador += é " + xd;
+}
+atribuicao();
+
+function atribuicao1(){
+    let xp = 10;
+    xp -= 5;
+    document.getElementById('sombra7').innerHTML = "O operador -= é " + xp;
+}
+atribuicao1();
+
+function atribuicao2(){
+    let xl = 10;
+    xl *= 5;
+    document.getElementById('sombra8').innerHTML = "O operador *= é " + xl;
+}
+atribuicao2();
+
+function atribuicao3(){
+    let xv = 10;
+    xv /= 5;
+    document.getElementById('sombra9').innerHTML = "O operador /= é " + xv;
+}
+atribuicao3();
+
+function atribuicao4(){
+    let xj = 10;
+    xj %= 5;
+    document.getElementById('sombra10').innerHTML = "O operador %= é " + xj;
+}
+atribuicao4();
+
+function atribuicao5() {
+    let xu = 10;
+    xu **= 5;
+    document.getElementById("sombra11").innerHTML =
+        "O operador **= é " + xu;
+}
+atribuicao5();
+
+function atribuicao6() {
+    let xup = 10;
+    document.getElementById("sombra12").innerHTML =
+        "O operador == é " + (xup == 50);
+}
+atribuicao6();
+
+function atribuicao7() {
+    let xuo = 10;
+    document.getElementById("sombra13").innerHTML =
+        "O operador === é " + (xuo === 50);
+}
+atribuicao7();
+
+function atribuicao8() {
+    let xuop = 10;
+    document.getElementById("sombra14").innerHTML =
+        "O operador != é " + (xuop != 50);
+}
+atribuicao8();
+
+function atribuicao9() {
+    let xuopx = 10;
+    document.getElementById("sombra15").innerHTML =
+        "O operador !== é " + (xuopx !== 50);
+}
+atribuicao9();
+
+function atribuicao10() {
+    let xuopxq = 10;
+    document.getElementById("sombra16").innerHTML =
+        "O operador < é " + (xuopxq < 50);
+}
+atribuicao10();
+
+function atribuicao11() {
+    let xuopxqw = 10;
+    document.getElementById("sombra17").innerHTML =
+        "O operador >= é " + (xuopxqw >= 50);
+}
+atribuicao11();
+
+function atribuicao12() {
+    let xuopxqa = 10;
+    document.getElementById("sombra18").innerHTML =
+        "O operador <= é " + (xuopxqa <= 50);
+}
+atribuicao12();
