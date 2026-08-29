4bb49416ad28c2c5eaed6f35dc397c880daf30cf
Matheus Nunes <matheus.brisanet@gmail.com>
2026-08-29 14:53:02 -0300

    Na pasta curso em vídeo foi craido um arquivo tipo primitivos em Python

---
+++ cursoEmVIdeo/python/2-Tratando dados e fazendo contas/1-Aula 6 – Tipos Primitivos e Saída de Dados.py
@@ -0,0 +1,14 @@
+# Aula 6 – Tipos Primitivos e Saída de Dados
+
+num1 = int(input('Digite um número: '));
+num2 = int(input('Digite outro número: '));
+sum = num1 + num2;
+print('A soma de {} + {} é {}'.format(num1, num2, sum));
+
+"""
+int = 7, -4, 0, 9875;
+float = 4.5, 0.076, -15.223, 7.0;
+bool = True, False;
+string  = 'Olá', '7.5', '';
+"""
+
---
+++ cursoEmVIdeo/python/2-Tratando dados e fazendo contas/1.1-Exercício 4 – Dissecando uma Variável.py
@@ -0,0 +1,8 @@
+algo = input('Digite algo: ');
+print('O tipo primitivo desse valor é'. format(type(algo)));
+print('Só tem espaço? {}'.format( algo.isspace()));
+print('É um número? {}'.format( algo.isnumeric()));
+print('É alfabético? {}'.format( algo.isalpha()));
+print('É alfanumérico? {}'.format( algo.isalnum()));
+print('Está em maiúscula? {}'.format( algo.isupper()));
+print('Está em msnúscula?  {}' .format(algo.islower()));
